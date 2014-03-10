dc92e1fd99b58bffb9e508bfa749afdec13f22ea
Rich Bowman <BostonREB99@gmail.com>
2014-03-10 14:10:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pixtr::Application.config.secret_key_base = '72396727466810e62a85c4cc4ec7febff16ef873b615569169c328ab3f84303e339938604bd48c7a42bb226df4147af5d9e37a34a2456a1251859e768b767542'
