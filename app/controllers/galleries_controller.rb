c5144eb0105f8c6da8720dff619324d20e8790a1
Rich Bowman <BostonREB99@gmail.com>
2014-03-13 17:55:28 -0400

    implemented security

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -2,7 +2,7 @@ class GalleriesController < ApplicationController
   def index   #actions are just methods.  Here it's the action "index"
     #any method inside the Controller is an "Action"
     ## renders a view, in this case the "index" view.
-    @galleries = Gallery.all  
+    @galleries = current_user.galleries  #just gives galleries for current_user  
   end
 
   def show
@@ -14,22 +14,22 @@ class GalleriesController < ApplicationController
   end 
 
   def create
-    @gallery = Gallery.create(gallery_params)
-    redirect_to gallery_path(@gallery.id)
+    gallery = current_user.galleries.create(gallery_params)  #creates gallery that belongs to user
+    redirect_to gallery_path(gallery)
   end 
 
   def edit
-    @gallery = Gallery.find(params[:id])
+    @gallery = current_user.galleries.find(params[:id])
   end
 
   def update
-    gallery = Gallery.find(params[:id])
+    gallery = current_user.galleries.find(params[:id])
     gallery.update(gallery_params)
-    redirect_to gallery_path(@gallery)
+    redirect_to gallery
   end
 
   def destroy
-    gallery = Gallery.find(params[:id])
+    gallery = current_user.galleries.find(params[:id])
     gallery.destroy
     redirect_to root_path
   end
