3aabde5212258900c763a59ead8249d98a2362ec
Rich Bowman <BostonREB99@gmail.com>
2014-03-20 08:56:59 -0400

    membership relationships added

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -2,7 +2,7 @@ class FollowingRelationshipsController < ApplicationController
 
   def create
     followed_user = User.find(params[:id])
-    current_user.following_relationships.create(followed_user_id: followed_user.id)
+    current_user.follow followed_user
     redirect_to followed_user
   end
 end
